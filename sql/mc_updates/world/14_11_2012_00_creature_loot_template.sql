31f457464585086ad771a8994c2d28ffc8a630c7
Nebukad <tobithespeady@web.de>
2012-11-14 23:35:29 +0100

    Fixe Drop Chance Geisterperlen der Winterfelle

---
+++ sql/mc_updates/world/14_11_2012_00_creature_loot_template.sql
@@ -0,0 +1,9 @@
+-- Fixe Drop Chance von http://de.wowhead.com/item=21383
+
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance`=50 WHERE  `entry`=10738 AND `item`=21383 LIMIT 1;
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance`=50 WHERE  `entry`=10916 AND `item`=21383 LIMIT 1;
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance`=50 WHERE  `entry`=7438 AND `item`=21383 LIMIT 1;
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance`=50 WHERE  `entry`=7439 AND `item`=21383 LIMIT 1;
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance`=50 WHERE  `entry`=7440 AND `item`=21383 LIMIT 1;
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance`=50 WHERE  `entry`=7441 AND `item`=21383 LIMIT 1;
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance`=50 WHERE  `entry`=7442 AND `item`=21383 LIMIT 1;
