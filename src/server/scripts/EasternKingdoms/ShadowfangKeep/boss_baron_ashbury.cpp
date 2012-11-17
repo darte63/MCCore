ac7ecfb754ba23a953de589cf1f2a5cbdd6808f0
Monkey <monkey@mcwow.net>
2012-11-17 15:30:19 +0100

    Scripts/ShadowfangKeep: Phasenübergang 1->2

--- src/server/scripts/EasternKingdoms/ShadowfangKeep/boss_baron_ashbury.cpp
+++ src/server/scripts/EasternKingdoms/ShadowfangKeep/boss_baron_ashbury.cpp
@@ -1,13 +1,19 @@
 #include "ScriptPCH.h"
 #include "shadowfang_keep.h"
 
-enum eSpels {
+enum eSpells {
     SPELL_PAIN_AND_SUFFERING            = 93581,
     SPELL_PAIN_AND_SUFFERING_H          = 93581,
     SPELL_ASPHYXIATE                    = 93423,
     SPELL_ASPHYXIATE_H                  = 93423,
     SPELL_STAY_OF_EXECUTION             = 93468,
     SPELL_STAY_OF_EXECUTION_H           = 93468,
+    SPELL_DARK_ARCHANGEL_FORM           = 93757,
+    SPELL_CALAMITY                      = 93812
+};
+
+enum eAuras {
+    AURA_DARK_ARCHANGEL_FORM_VISUAL     = 93766
 };
 
 class boss_baron_ashbury : public CreatureScript
@@ -31,12 +37,14 @@ public:
         uint32 uiPainAndSufferingTimer;
         uint32 uiAsphyxiateTimer;
         uint32 uiStayOfExecutionTimer;
+        uint32 phase;
         
         void Reset()
         {
             uiPainAndSufferingTimer = 10*IN_MILLISECONDS;
             uiAsphyxiateTimer = 45*IN_MILLISECONDS;
             uiStayOfExecutionTimer = 57*IN_MILLISECONDS;
+            phase = 1;
         }
 
         void EnterCombat(Unit* /*pWho*/) {}
@@ -48,30 +56,51 @@ public:
             if (!UpdateVictim())
                 return;
             
-            if (uiPainAndSufferingTimer <= Diff)
+            if (phase == 1)
             {
-                if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 0, 150, true))
-                    me->CastSpell(target, DUNGEON_MODE(SPELL_PAIN_AND_SUFFERING, SPELL_PAIN_AND_SUFFERING_H), true);
+                if (uiPainAndSufferingTimer <= Diff)
+                {
+                    if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 0, 150, true))
+                        me->CastSpell(target, DUNGEON_MODE(SPELL_PAIN_AND_SUFFERING, SPELL_PAIN_AND_SUFFERING_H), true);
 
-                uiPainAndSufferingTimer = urand(10*IN_MILLISECONDS, 20*IN_MILLISECONDS); 
-            } else uiPainAndSufferingTimer -= Diff;
-            
-            if (uiAsphyxiateTimer <= Diff)
-            {
-                if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 0, 150, true))
-                    me->CastSpell(target, DUNGEON_MODE(SPELL_ASPHYXIATE, SPELL_ASPHYXIATE_H), true);
+                    uiPainAndSufferingTimer = urand(10*IN_MILLISECONDS, 20*IN_MILLISECONDS); 
+                } else uiPainAndSufferingTimer -= Diff;
 
-                uiAsphyxiateTimer = 45*IN_MILLISECONDS; 
-                uiPainAndSufferingTimer = 13*IN_MILLISECONDS; 
-            } else uiAsphyxiateTimer -= Diff;
+                if (uiAsphyxiateTimer <= Diff)
+                {
+                    if (Unit* target = SelectTarget(SELECT_TARGET_RANDOM, 0, 150, true))
+                        me->CastSpell(target, DUNGEON_MODE(SPELL_ASPHYXIATE, SPELL_ASPHYXIATE_H), true);
+
+                    uiAsphyxiateTimer = 45*IN_MILLISECONDS; 
+                    uiPainAndSufferingTimer = 13*IN_MILLISECONDS; 
+                } else uiAsphyxiateTimer -= Diff;
+
+                if (uiStayOfExecutionTimer <= Diff)
+                {
+                    DoCast(me, DUNGEON_MODE(SPELL_STAY_OF_EXECUTION,SPELL_STAY_OF_EXECUTION_H));
+                    uiStayOfExecutionTimer = 45*IN_MILLISECONDS;
+                    uiPainAndSufferingTimer = 10*IN_MILLISECONDS; 
+                } else uiStayOfExecutionTimer -= Diff;
+                
+                if (phase == 1 && me->GetHealthPct() <= 25.0f && IsHeroic())
+                {
+                    DoCast(me, DUNGEON_MODE(0, SPELL_DARK_ARCHANGEL_FORM));
+                }
+                
+                if (me->HasAura(SPELL_DARK_ARCHANGEL_FORM) && IsHeroic())
+                {
+                    phase = 2;
+                    me->AddAura(DUNGEON_MODE(0, AURA_DARK_ARCHANGEL_FORM_VISUAL), me);
+                    DoCast(me, DUNGEON_MODE(0, SPELL_DARK_ARCHANGEL_FORM));
+                }      
+            }
             
-            if (uiStayOfExecutionTimer <= Diff)
+            if (phase == 2 && IsHeroic())
             {
-                DoCast(me, DUNGEON_MODE(SPELL_STAY_OF_EXECUTION,SPELL_STAY_OF_EXECUTION_H));
-                uiStayOfExecutionTimer = 45*IN_MILLISECONDS;
-                uiPainAndSufferingTimer = 10*IN_MILLISECONDS; 
-            } else uiStayOfExecutionTimer -= Diff;
+                // ToDo: Phase 2
+            }
             
+            if (phase)
             DoMeleeAttackIfReady();
         }
     };
