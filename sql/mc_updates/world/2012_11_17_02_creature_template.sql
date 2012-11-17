883e4bac8f9b0346e6250c18abd2421acf850454
Monkey <monkey@mcwow.net>
2012-11-17 15:34:19 +0100

    SQL/World: Korrigiere SQL

--- sql/mc_updates/world/2012_11_17_02_creature_template.sql
+++ sql/mc_updates/world/2012_11_17_02_creature_template.sql
@@ -1,1 +1,1 @@
-UPDATE creature_template SET EventAI = '', ScriptName = 'boss_baron_ashbury' WHERE entry = 46962;
+UPDATE creature_template SET AIName = '', ScriptName = 'boss_baron_ashbury' WHERE entry = 46962;
