7486f6754bd225f01497e0d8a2e22d8a5ec0fe6a
Nebukad <tobithespeady@web.de>
2012-11-18 21:51:34 +0100

    DB/Creature: Anpassung vom Leben im Vortexgipfel

---
+++ sql/mc_updates/world/2012_11_18_07_creature_template.sql
@@ -0,0 +1,7 @@
+-- Anpassung von Leben der Trashmobs im Vortexgipfel
+UPDATE `creature_template` SET `Health_mod`=7.21769 WHERE  `entry`=45926 LIMIT 1;
+UPDATE `creature_template` SET `Health_mod`=8.3 WHERE  `entry`=45927 LIMIT 1;
+UPDATE `creature_template` SET `Health_mod`=5 WHERE  `entry`=45935 LIMIT 1;
+UPDATE `creature_template` SET `Health_mod`=7 WHERE  `entry`=45929 LIMIT 1;
+UPDATE `creature_template` SET `Health_mod`=3 WHERE  `entry`=45933 LIMIT 1;
+UPDATE `creature_template` SET `Health_mod`=30 WHERE  `entry`=43874 LIMIT 1;
