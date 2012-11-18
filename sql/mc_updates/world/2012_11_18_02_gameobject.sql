7c439279c277a4f5e825418ed427ec94d88bed5c
Nebukad <tobithespeady@web.de>
2012-11-18 17:01:09 +0100

    DB/Gobject: Lösche Portale die überflüssig in SW standen (Tol Barad(Alle auf einen Fleck))

---
+++ sql/mc_updates/world/2012_11_18_02_gameobject.sql
@@ -0,0 +1,16 @@
+-- Lösche überflüssige Portale in SW!
+DELETE FROM `gameobject` WHERE  `guid`=4018084 LIMIT 1;
+DELETE FROM `gameobject` WHERE  `guid`=4018893 LIMIT 1;
+DELETE FROM `gameobject` WHERE  `guid`=4019706 LIMIT 1;
+DELETE FROM `gameobject` WHERE  `guid`=4020486 LIMIT 1;
+DELETE FROM `gameobject` WHERE  `guid`=4020781 LIMIT 1;
+DELETE FROM `gameobject` WHERE  `guid`=4022235 LIMIT 1;
+DELETE FROM `gameobject` WHERE  `guid`=4022584 LIMIT 1;
+DELETE FROM `gameobject` WHERE  `guid`=4022756 LIMIT 1;
+DELETE FROM `gameobject` WHERE  `guid`=4023439 LIMIT 1;
+DELETE FROM `gameobject` WHERE  `guid`=4023933 LIMIT 1;
+DELETE FROM `gameobject` WHERE  `guid`=4024476 LIMIT 1;
+DELETE FROM `gameobject` WHERE  `guid`=4025195 LIMIT 1;
+DELETE FROM `gameobject` WHERE  `guid`=4026937 LIMIT 1;
+DELETE FROM `gameobject` WHERE  `guid`=50000 LIMIT 1;
+
