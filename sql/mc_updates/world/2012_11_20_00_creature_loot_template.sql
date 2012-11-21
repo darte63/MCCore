e6b0ebbfbc70bb7d996db77cf39cea6452e4eb8f
Monkey <monkey@mcwow.net>
2012-11-21 16:17:11 +0100

    Revert "DB/Loot: Passe Loote von den Chaoskugeln an.... und etwas Stylefix"
    This reverts commit 827e513ef23c18f2721f3b4ab3f3b5340db38f51.

--- sql/mc_updates/world/2012_11_20_00_creature_loot_template.sql
+++
@@ -1,9 +0,0 @@
--- Passe Dropchance der Chaoskugeln in den 5er NONHC ! an. (nicht blizzlike)
-INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`) VALUES (42333, 52078, 50);
-INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`) VALUES (43875, 52078, 50);
-INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`) VALUES (44819, 52078, 50);
-INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`) VALUES (40484, 52078, 50);
-INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`) VALUES (39705, 52078, 50);
-INSERT INTO `creature_loot_template` (`entry`, `item`, `ChanceOrQuestChance`) VALUES (39378, 52078, 50);
-
-_
