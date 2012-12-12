4bdfac5546673dca89c5c79427418774af9258fe
Nebukad <tobithespeady@web.de>
2012-12-12 22:58:10 +0100

    DB/Quest: Fixe Quest Disziplin

---
+++ sql/mc_updates/world/2012_12_12_00_quest_template.sql
@@ -0,0 +1,3 @@
+-- Fixe Quest Disziplin
+UPDATE `quest_template` SET `ReqSpellCast1`=56033 WHERE  `entry`=12906 LIMIT 1;
+UPDATE `quest_template` SET `ReqCreatureOrGOId1`=30146 WHERE  `entry`=12906 LIMIT 1;
